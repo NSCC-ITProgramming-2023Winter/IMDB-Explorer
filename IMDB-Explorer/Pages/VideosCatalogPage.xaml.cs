8960b29232f26c00eca296b1ebe89fc81b77d36f
walton <zjxcdut@gmail.com>
2024-12-05 23:11:08 -0400

    Add linq search functionality in VideosCatalog Page.

--- IMDB-Explorer/Pages/VideosCatalogPage.xaml.cs
+++ IMDB-Explorer/Pages/VideosCatalogPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using IMDB_Explorer.Data;
+using IMDB_Explorer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,38 @@ namespace IMDB_Explorer.Pages
     /// </summary>
     public partial class VideosCatalogPage : Page
     {
+        private readonly ImdbContext _context = new ImdbContext();
+        private CollectionViewSource videosCatalogViewSource = new CollectionViewSource();
         public VideosCatalogPage()
         {
             InitializeComponent();
+            videosCatalogViewSource = (CollectionViewSource)FindResource(nameof(videosCatalogViewSource));
+
+            //Load data from the database
+            _context.Names.Load();
+            //_context.Artists.Load();
+            //_context.Albums.Load();
+            //_context.Tracks.Load();
+
+            videosCatalogViewSource.Source = _context.Names.Local.ToObservableCollection();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //Define a grouping query to get grouped category data
+            var query =
+                from name in _context.Names
+                where name.PrimaryName.Contains(textSearch.Text)
+                select new
+                {
+                    name.NameId,
+                    name.PrimaryName,
+                    name.BirthYear,
+                    name.DeathYear
+                };
+
+            //Execture the query against the db and assign it as the data source for the listview
+            videosCatalogListVew.ItemsSource = query.ToList();
         }
     }
 }
