7fdfde8caec36f214047746a61496462781622c6
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-12-07 02:53:14 -0400

    Integrate CollectionViewSource and enhance search functionality
    Added a CollectionViewSource in WritersListPage.xaml for data binding.
    Updated the search bar TextBox to include a FontSize property.
    Modified ListView to bind to writer properties and renamed it.
    Added necessary using directives in WritersListPage.xaml.cs.
    Introduced private fields for ImdbContext and CollectionViewSource.
    Initialized writerViewSource and called LoadInitialData in constructor.
    Implemented LoadInitialData to load data and set writerViewSource.
    Updated textSearch_TextChanged to filter writers list using LINQ.
    Added SQL query in SQLQuery2.sql to retrieve distinct writer info.

--- IMDB-Explorer/Pages/WritersListPage.xaml.cs
+++ IMDB-Explorer/Pages/WritersListPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IMDB_Explorer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,13 +22,73 @@ namespace IMDB_Explorer.Pages
     /// </summary>
     public partial class WritersListPage : Page
     {
+        
+
+        private ImdbContext _context = new ImdbContext();
+        CollectionViewSource writerViewSource = new CollectionViewSource();
+        
         public WritersListPage()
         {
             InitializeComponent();
+
+            //Tie the markup viewsource object to the C# code viewsource object
+            writerViewSource = (CollectionViewSource)FindResource(nameof(writerViewSource));
+
+
+            LoadInitialData();
+
+
+            //Use the dbContext to tell EF to load data from a junction between Principal and Name tables for our Writers data
+            //_context.Names.Load();
+            // Bind the data to the grid
+            //writerViewSource.Source = writers;
+
+            //Bind the initial list of Names to the listbox
+            //listWriterSearchResults.ItemsSource = writerViewSource;
+        }
+
+        private void LoadInitialData()
+        {
+            //Linq
+            writerViewSource.Source = _context.Principals 
+                .Where(p => p.JobCategory == "writer")
+                .Join(_context.Names,
+                      principal => principal.NameId,
+                      name => name.NameId,
+                      (principal, name) => new
+                      {
+                          Name = name.PrimaryName,
+                          BirthYear = name.BirthYear,
+                          DeathYear = name.DeathYear
+                      })
+                .Distinct()
+                .OrderBy(x => x.Name)
+                .ToList();
         }
 
         private void textSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
+            //Linq
+            //Defining our LINQ Query 
+
+            var query = _context.Principals
+            .Where(p => p.JobCategory == "writer")
+            .Join(_context.Names,
+          principal => principal.NameId,
+          name => name.NameId,
+          (principal, name) => new
+          {
+              Name = name.PrimaryName,
+              BirthYear = name.BirthYear,
+              DeathYear = name.DeathYear
+          })
+            .Where(n => n.Name.ToLower().Contains(textSearch.Text.ToLower()))
+            .Distinct()
+            .OrderBy(n => n.Name)
+            .ToList();
+
+            //Update the listbox with the results of the query
+            listWriterSearchResults.ItemsSource = query;
 
         }
     }
---
+++ SQLQuery2.sql
@@ -0,0 +1,19 @@
+﻿SELECT DISTINCT
+   p.primaryName AS Name, 
+   p.birthYear AS BirthYear, 
+   p.deathYear AS DeathYear
+FROM 
+   Names p 
+JOIN 
+   Principals n
+ON 
+   p.nameID = n.nameID
+WHERE 
+   n.job_category = 'writer'
+
+ ORDER BY 
+   p.primaryName 
+
+SELECT * FROM Principals; 
+
+SELECT * FROM Names;
