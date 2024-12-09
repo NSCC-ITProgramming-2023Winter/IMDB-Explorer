f29b2d0e4085d87f097ac09e5ef1bfe4509fcf17
walton <zjxcdut@gmail.com>
2024-12-09 11:37:37 -0400

    Add Directors List Page and fix some bug in Videos Catalog Page

--- IMDB-Explorer/Pages/DirectorsListPage.xaml.cs
+++ IMDB-Explorer/Pages/DirectorsListPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IMDB_Explorer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,46 @@ namespace IMDB_Explorer.Pages
     /// </summary>
     public partial class DirectorsListPage : Page
     {
+        private readonly ImdbContext _context = new ImdbContext();
+        private CollectionViewSource directorsViewSource = new CollectionViewSource();
+
         public DirectorsListPage()
         {
             InitializeComponent();
+
+            /* 
+             * This will load all the Names
+            //directorsViewSource = (CollectionViewSource)FindResource(nameof(directorsViewSource));
+
+            // Use the dbContext to tell EF to load the data we want to use on this page
+            //_context.Names.Load();
+
+            // Set the viewsource data source to use the Products data collection (dbset)
+            //directorsViewSource.Source = _context.Names.Local.ToObservableCollection();
+            */
+
+            // Only Load directors
+            LoadDirectors();
+        }
+
+        private void LoadDirectors()
+        {
+            var directors = _context.Names
+                                    .Where(n => n.PrimaryProfession != null && n.PrimaryProfession.Contains("director"))
+                                    .ToList();
+
+            directorsListView.ItemsSource = directors;
+        }
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var searchQuery = textSearch.Text.ToLower();
+
+            var directors = _context.Names
+                                    .Where(n => n.PrimaryProfession != null && n.PrimaryProfession.Contains("director")
+                                            && n.PrimaryName.ToLower().Contains(searchQuery))
+                                    .ToList();
+
+            directorsListView.ItemsSource = directors;
         }
     }
 }
--- IMDB-Explorer/Pages/VideosCatalogPage.xaml.cs
+++ IMDB-Explorer/Pages/VideosCatalogPage.xaml.cs
@@ -2,19 +2,10 @@
 using IMDB_Explorer.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IMDB_Explorer.Pages
 {
@@ -24,57 +15,53 @@ namespace IMDB_Explorer.Pages
     public partial class VideosCatalogPage : Page
     {
         private readonly ImdbContext _context = new ImdbContext();
-        private CollectionViewSource videosCatalogViewSource = new CollectionViewSource();
+
         public VideosCatalogPage()
         {
             InitializeComponent();
-            videosCatalogViewSource = (CollectionViewSource)FindResource(nameof(videosCatalogViewSource));
-
-            //Load data from the database
-            //_context.Names.Load(); This will load all the data,  too slow
-            //_context.Artists.Load();
-            //_context.Albums.Load();
-            //_context.Tracks.Load();
-
-            LoadInitialData(); // use this function, only load 100 row data
-
-            videosCatalogViewSource.Source = _context.Names.Local.ToObservableCollection();
         }
-
-        private void LoadInitialData()
-        {
-            // Load only 100 pieces of data for the initial
-            videosCatalogViewSource.Source = _context.Names
-                .Include(n => n.Titles) // Explicit Loading of related data (Titles)
-                .OrderBy(n => n.NameId)
-                .Take(100) // Control the number of loads
-                .ToList();
-        }
-
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            //Define a grouping query to get grouped category data
-            var query =
-                from name in _context.Names
-                where name.PrimaryName.Contains(textSearch.Text)
-                group name by name.PrimaryName.ToUpper().Substring(0, 1) into nameGroup
-                select new
-                {
-                    Index = nameGroup.Key,
-                    ArtCount = nameGroup.Count().ToString(),
-                    name = nameGroup.ToList<Name>()
-                };
-            //select new
-            //{
-            //    name.NameId,
-            //    name.PrimaryName,
-            //    name.BirthYear,
-            //    name.DeathYear
-            //};
+            // Get search input
+            var searchText = textSearch.Text.Trim();
 
-            //Execture the query against the db and assign it as the data source for the listview
-            videosCatalogListVew.ItemsSource = query.ToList();
+            // If the input is empty, display a prompt or clear the result
+            if (string.IsNullOrEmpty(searchText))
+            {
+                videosCatalogListVew.ItemsSource = null;
+                MessageBox.Show("Please enter a search term.", "No Input", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            // Push the query to the database and load the relevant data directly
+            var query = _context.Names
+                .Where(name => name.PrimaryName.Contains(searchText)) 
+                .Include(name => name.Titles)  // Explicitly load Titles
+                .GroupBy(name => name.PrimaryName.Substring(0, 1).ToUpper()) 
+                .Select(group => new
+                {
+                    Index = group.Key,
+                    ArtCount = group.Count(),
+                    name = group.Select(n => new
+                    {
+                        n.NameId,
+                        n.PrimaryName,
+                        Titles = n.Titles.Select(t => new { t.PrimaryTitle }).ToList()
+                    }).ToList()
+                })
+                .ToList(); // Query Execution
+
+            // Checking Titles data
+            //foreach (var item in query)
+            //{
+            //    foreach (var name in item.name)
+            //    {
+            //        Debug.WriteLine($"Name: {name.PrimaryName}, Titles Count: {name.Titles.Count}");
+            //    }
+            //}
+
+            videosCatalogListVew.ItemsSource = query;
         }
     }
 }
